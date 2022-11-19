9136719314c27a94fcdc2bf525f4fb118a4c87c8
Shree <Shree@DESKTOP-5GK796K>
2022-11-19 06:09:34 -0800

    commit 4th

--- src/string/CharacterCountOccurance.java
+++ src/string/CharacterCountOccurance.java
@@ -6,7 +6,7 @@ public static void main(String[] args)
 {
 	String s="nitin pund";
 	int totalCount =s.length();
-	int totalCount_afterRemove=s.replace("n", "").length();
+	int totalCount_afterRemove=s.replace("i", "").length();
 	int count=totalCount-totalCount_afterRemove;
 	System.out.println("Number of Occurances of n is : "+count);
 }
