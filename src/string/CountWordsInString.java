395d42964c1138c0e95fb7843e1747bbadbf3295
Shree <Shree@DESKTOP-5GK796K>
2022-11-20 02:11:31 -0800

    5th commit

--- src/string/CountWordsInString.java
+++ src/string/CountWordsInString.java
@@ -4,7 +4,7 @@ public class CountWordsInString
 {
 public static void main(String[] args)
 {
-	String s="Nitin Pund";
+	String s="Nitin S Pund";
 	int totalCount =s.length();
 	System.out.println("Number of word is giver String : "+ totalCount);
 }
