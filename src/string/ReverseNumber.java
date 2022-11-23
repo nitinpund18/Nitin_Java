5a11960f12d4209bec7dd07bf2beb6ad9a17d92c
Shree <Shree@DESKTOP-5GK796K>
2022-11-23 01:17:07 -0800

    commit no 7

--- src/string/ReverseNumber.java
+++ src/string/ReverseNumber.java
@@ -4,7 +4,7 @@ public class ReverseNumber
 {
 public static void main(String[] args) 
 {
-		int[]a= {1,2,3,4,5,6,7,8,9};
+		int[]a= {1,2,3,4,5,6,7,8};
 		System.out.print("Original array:");
 		for(int i=0;i<a.length;i++)
 		{
